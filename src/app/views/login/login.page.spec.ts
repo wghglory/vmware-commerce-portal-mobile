1c400b462d9802555b5de778d1826d89ed42c76c
Derek Wang <guanghui-wang@foxmail.com>
2021-04-04 11:12:14 +0800

    style: format project

--- src/app/views/login/login.page.spec.ts
+++ src/app/views/login/login.page.spec.ts
@@ -17,7 +17,7 @@ describe('LoginPage', () => {
       fixture = TestBed.createComponent(LoginPage);
       component = fixture.componentInstance;
       fixture.detectChanges();
-    })
+    }),
   );
 
   it('should create', () => {
